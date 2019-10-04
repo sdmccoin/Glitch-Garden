87ad1bc55ed96f7ed0a73c1a4cbe2f252d7cc04a
smccoin <stevendmccoin@msn.com>
2019-10-04 06:02:24 -0400

    added gravestone and gnome

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,7 +6,49 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetLaneSpawner();
+    }
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            animator.SetBool("IsAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            // is the spawner "basically" on the same y axis as the gameobject shooting (taking the absolute values of y (meaning positive or negative numbers, making them borth positive)
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+            return false;
+        else
+            return true;
+    }
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, transform.rotation);
